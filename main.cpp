4ff721a9e8e8271712c06fd9ea5be8c7e5bdb4be
Dhiraj Girdhar <girdhar.dhiraj@gmail.com>
2020-09-14 21:59:16 -0400

    Update main.cpp
    In this version, Instead of checking all conditions at once, I'm checking one condition before running 3rd loop, it improved my performance drastically. I got the following results.
    Runtime: 32 ms, faster than 82.27% of C++ online submissions for Count Good Triplets.
    Memory Usage: 8.2 MB, less than 74.78% of C++ online submissions for Count Good Triplets.

--- main.cpp
+++ main.cpp
@@ -5,11 +5,12 @@ public:
         
         for (int i = 0; i < arr.size(); ++i) {
             for (int j = i + 1; j < arr.size(); ++j) {
-                for (int k = j + 1; k < arr.size(); ++k) {
-                    if (abs(arr[i] - arr[j]) <= a && 
-                        abs(arr[j] - arr[k]) <= b && 
-                        abs(arr[i] - arr[k]) <= c) {
-                        count++;
+                if (abs(arr[i] - arr[j]) <= a) {
+                    for (int k = j + 1; k < arr.size(); ++k) {
+                        if (abs(arr[j] - arr[k]) <= b && 
+                            abs(arr[i] - arr[k]) <= c) {
+                            count++;
+                        }
                     }
                 }
             }
